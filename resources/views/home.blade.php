05093a0954c3e61a0f25d62fddb9df47c1ecdee5
Hildleybrando <infra-2000@hotmail.com>
2023-08-07 22:23:20 -0300

    Adicionados alguns estilos no styles.css e primeira parte da página inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+
+@extends('layouts.main')
+@section('title','MyProjects')
+
+@section('content')
+<div>
+   <div class="container text-center">
+        <div style="margin-top: 20px;">
+            <h1>SEJA BEM VINDO!</h1>
+            <p><h3>Esta página é apenas uma introdução. Os links no Menu levarão ao verdadeiro conteúdo deste projeto.</h3></p>
+            <p><h3>Ao efetuar o login, o conteúdo da página irá ser apresentado em forma de vitrine.</h3></p>
+        </div>   
+   </div>
+</div>
+
+@endsection
+
+
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="home" content="width=device-width, initial-scale=1">
+        
+        <title>@yield('title')</title>
+        <!-- fontes do google -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- css -->
+        <link rel="stylesheet" href="/css/styles.css">
+
+        <!-- css bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+        
+        <!-- javascript -->
+        <script src="/js/scripts.js"></script>
+        
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid" id="navbar">
+                    <a href="/" class="navbar-brand">
+                        <img src="/img/logoMP.png" alt="logo"><b>MyProjects</b>
+                    </a>
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/"><b>Projetos</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/"><b>Entrar</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/"><b>Cadastrar</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/"><b>Sair</b></a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        @yield('content')
+        <footer class="bd-footer py-4 py-md-5 mt-5" id="marca"><b>MyProjects&copy; 2023</b>
+        </footer>
+    </body>
+
+</html>
